30087ac312b308e0aba0381b921f60be0e226c0a
kproudler <57147062+kproudler@users.noreply.github.com>
2022-02-02 17:27:55 -0500

    1samp ttest hd-chol

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_1samp
 
 
 heart = pd.read_csv('heart_disease.csv')
@@ -7,3 +8,14 @@ yes_hd = heart[heart.heart_disease == 'presence']
 no_hd = heart[heart.heart_disease == 'absence']
 
 # print(yes_hd, no_hd)
+
+chol_hd = yes_hd.chol
+
+mean_chol_hd = np.mean(chol_hd)
+# mean chol from ppl with hd = ~251
+print(mean_chol_hd)
+# null hypothesis: People with hd have an avg chol = 240
+# alt hypothesis: People with hd have avg chol > 240
+tstat, pvalue = ttest_1samp(chol_hd, 240, alternative='greater')
+print(tstat, pvalue)
+
