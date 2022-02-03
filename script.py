32aee56eff963cd42f129c967297d3fea948cf0b
kproudler <57147062+kproudler@users.noreply.github.com>
2022-02-03 08:18:16 -0500

    ttest chol_no_hd

--- script.py
+++ script.py
@@ -17,5 +17,13 @@ print(mean_chol_hd)
 # null hypothesis: People with hd have an avg chol = 240
 # alt hypothesis: People with hd have avg chol > 240
 tstat, pvalue = ttest_1samp(chol_hd, 240, alternative='greater')
+# p-value of .00354 - reject null hypothesis
 print(tstat, pvalue)
 
+# hypothesis test for patients not diagnosed with hd
+# null hyp: People w/o hd have avg chol = 240
+# alt hyp: avg chol > 240
+chol_no_hd = no_hd.chol
+# pvalue = .26, do not reject null hypothesis
+tstat, pvalue = ttest_1samp(chol_no_hd, 240, alternative='greater')
+print(tstat, pvalue)
